cb50eab947ac2abc1d0f55f4c8eb3fc74ccc6c92
paoloverasteguiflorenttini <paoloverasteguiflorenttini@paolos-mbp>
2024-01-03 21:03:16 -0500

    Initial Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,14 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String[] args) {
+		System.out.println("Wednesday");
+		
+		System.out.println("Batch11");
+		
+		System.out.println("Batch11 git");
+		
+	}
+
+}
